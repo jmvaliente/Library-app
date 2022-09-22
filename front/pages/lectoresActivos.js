e4ee3fefb35b3786908a2d03c59e50894461be1e
Kran <68863512+srKran@users.noreply.github.com>
2022-09-22 17:52:23 +0200

    Delete lectoresActivos.js

--- front/pages/lectoresActivos.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import LectorActivo from '../ui/components/LectorActivo'
-
-export default function lectoresActivos () {
-  return <div className="p-2 md:32 lg:48"><LectorActivo/></div>
-}
