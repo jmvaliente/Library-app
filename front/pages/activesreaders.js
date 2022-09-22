272b4f7f186591a26610c1790822de1e2a4a1614
Kran <68863512+srKran@users.noreply.github.com>
2022-09-22 17:53:04 +0200

    Añadido pagina de Lectores Activos

---
+++ front/pages/activesreaders.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ActiveReader from '../ui/components/ActiveReader'
+
+export default function activesreaders () {
+  return <div className="p-2 md:32 lg:48"><ActiveReader/></div>
+}
