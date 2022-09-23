4578c31ae18075e7051500ed2c544477cd8c7baa
Miguel Decode <migueldecode@gmail.com>
2022-09-23 11:43:49 +0200

    feat: menu mobile
    Create a component menu-mobile with four icons

--- front/pages/authorpage.js
+++ front/pages/authorpage.js
@@ -1,12 +1,10 @@
-import React from "react";
-import Author from "../ui/components/Author";
-import MenuMobile from "../ui/components/MenuMobile";
+import React from 'react'
+import Author from '../ui/components/Author'
 
-export default function authorPage() {
+export default function authorPage () {
   return (
     <div className="p-2 md:32 lg:48">
-      <MenuMobile />
       <Author />
     </div>
-  );
+  )
 }
--- front/ui/components/MenuMobile.jsx
+++ front/ui/components/MenuMobile.jsx
@@ -1,27 +1,61 @@
-import React from "react";
-import Image from "next/image";
+import React from 'react'
+import Image from 'next/image'
 
-function MenuMobile() {
+function MenuMobile () {
   return (
-    <nav className="fixed bottom-0">
-      <Image
-        loading="lazy"
-        className="object-cover object-center"
-        alt="Icon Menu"
-        src={"/../public/home.png"}
-        width={48}
-        height={48}
-      />
-      {/* <ul>
+    <nav className="fixed bottom-0 w-full rounded-t-xl border-2 border-slate-800 p-2 bg-white">
+      <ul className='flex justify-around'>
         <li>
-          <a>INICIO</a>
+          <a href='#'>
+            <Image
+              loading="lazy"
+              className="object-cover object-center"
+              alt="Icon Menu"
+              src={'/../public/home.png'}
+              width={40}
+              height={40}
+            />
+          </a>
         </li>
         <li>
-          <a>MI PERFIL</a>
+          <a href='#'>
+            <Image
+              loading="lazy"
+              className="object-cover object-center"
+              alt="Icon Menu"
+              src={'/../public/open-book.png'}
+              width={40}
+              height={40}
+            />
+          </a>
         </li>
-      </ul> */}
+        <li>
+          <a href='#'>
+            <Image
+              loading="lazy"
+              className="object-cover object-center"
+              alt="Icon Menu"
+              src={'/../public/search.png'}
+              width={40}
+              height={40}
+            />
+          </a>
+        </li>
+        <li>
+          <a href='#'>
+            <Image
+              loading="lazy"
+              className="object-cover object-center"
+              alt="Icon Menu"
+              src={'/../public/user.png'}
+              width={40}
+              height={40}
+            />
+          </a>
+        </li>
+      </ul>
     </nav>
-  );
+  )
 }
 
-export default MenuMobile;
+export default MenuMobile
