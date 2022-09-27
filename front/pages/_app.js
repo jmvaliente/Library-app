8c095bfdea6cdf9ecac5584f71e7680edeaeb7a0
David <davidesarrolloweb@gmail.com>
2022-09-27 19:34:44 +0200

    feat: section rating(valoracion estrellas)

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../styles/globals.css'
+import 'tailwindcss/tailwind.css'
 
 function MyApp ({ Component, pageProps }) {
   return <Component {...pageProps} />
--- front/ui/components/Book.jsx
+++ front/ui/components/Book.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import { BOOK_DETAIL_MOOK } from '../../config/mock/mock'
+import Rating from './rating'
 
 export default function Book () {
   return (
@@ -26,7 +27,7 @@ export default function Book () {
             </div>
             <div className="flex pt-4">
                 <div className="flex-1 w-50">
-                    4 Stars
+                    <Rating/>
                 </div>
                 <section className="flex-1 w-50 text-right">
                     <p>
