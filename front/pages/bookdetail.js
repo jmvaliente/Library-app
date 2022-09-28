be2d24a2af6560f4d08af51be2b7001a82c7bfde
jmvaliente <valiente_jose2@hotmail.com>
2022-09-28 23:59:08 +0200

    create service fetch data

--- front/pages/bookdetail.js
+++ front/pages/bookdetail.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import { useRouter } from 'next/router'
+import { useGetBook } from '../services/hooks/booksApi'
 import Book from '../ui/components/Book'
 
 export default function Bookdetail () {
+  const router = useRouter()
+  const { id } = router.query
+  const data = useGetBook(id)
+
   return (
     <div className="p-2 md:32 lg:48">
-        <Book/>
+        {data?.book.volumeInfo
+          ? <Book>{data}</Book>
+          : null}
     </div>
   )
 }
