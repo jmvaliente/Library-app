975bcc2721bf3542bcb07a8bf4a90ca25398afad
Efrain Jimenez <efrainj.ti@gmail.com>
2022-09-29 11:52:31 -0500

    feat: search books

--- front/ui/components/Search.jsx
+++ front/ui/components/Search.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import Image from "next/image";
 import { LISTBOOK } from "../../config/mock/mock";
 
-export default function Search() {
+export default function Search () {
   const { image } = LISTBOOK;
   return (
     <div className="buscar">
-      <input type="text" className="buscar-autor" id=""  placeholder="Buscador de Libro"/>
+      <input type="text" className="buscar-autor" id=""placeholder="Buscador de Libro"/>
       <div className="lupa">
         <button> <Image src={image} width={20} height={20} tabIndex={10} /></button>
       </div>
