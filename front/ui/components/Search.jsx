b916faea55f7c81de7e851333c8171560f4d4cdf
Miguel Decode <migueldecode@gmail.com>
2022-10-01 01:40:32 +0200

    design: Add tailwind styles to component

--- front/ui/components/Search.jsx
+++ front/ui/components/Search.jsx
@@ -5,11 +5,19 @@ import { LISTBOOK } from '../../config/mock/mock'
 export default function Search () {
   const { image } = LISTBOOK
   return (
-    <div className="buscar">
-      <input type="text" className="buscar-autor" id="" placeholder="Buscador de Libro" />
-      <div className="lupa">
+    <div className="flex gap-4">
+      <input type="text" className="bg-slate-300 text-white rounded-full pl-6 w-full placeholder:text-white focus:outline-slate-400" placeholder="Buscador de Libro" />
+      <Image
+              loading="lazy"
+              className=''
+              alt="Lupa"
+              src={image}
+              width={40}
+              height={40}
+            />
+      {/* <div className="lupa">
         <button> <Image src={image} width={20} height={20} tabIndex={10} /></button>
-      </div>
+      </div> */}
     </div>
   )
 }
