faab133639293c2dd0f68fb654feb1e817af0e65
Kran <Kran>
2022-10-01 04:08:38 +0200

    Add function in the search without styles

--- front/ui/components/Search.jsx
+++ front/ui/components/Search.jsx
@@ -1,14 +1,31 @@
-import React from 'react'
+import { useState, React } from 'react'
 import Image from 'next/image'
 import { LISTBOOK } from '../../config/mock/mock'
+import { useRouter } from 'next/router'
+
+function checkAnswer (event, value, router) {
+  if (event.key === 'Enter' || event.type === 'click') {
+    naviteTo(router, value)
+  }
+}
+function naviteTo (router, value) {
+  if (router.pathname === '/') {
+    //Si es desde el inicio, irá al bookList
+    router.push('/searchbooklist?id=' + value)
+  }
+}
 
 export default function Search () {
   const { image } = LISTBOOK
+  const [value, setValue] = useState('')
+  const router = useRouter()
   return (
     <div className="buscar">
-      <input type="text" className="buscar-autor" id="" placeholder="Buscador de Libro" />
+      <input value={value}
+      onChange={e => { setValue(e.currentTarget.value) }}
+      onKeyUp={(event) => { checkAnswer(event, value, router) } } type="text" name ="buscador" className="buscar-autor" id="name" placeholder="Buscador de Libro" />
       <div className="lupa">
-        <button> <Image src={image} width={20} height={20} tabIndex={10} /></button>
+        <button onClick={(event) => { checkAnswer(event, value, router) } } >  <Image src={image} width={20} height={20} tabIndex={10} /></button>
       </div>
     </div>
   )
